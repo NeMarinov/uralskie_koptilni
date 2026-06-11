a1d4158a53bd53a836234e0b81357796684a3b37
NeMarinov <od1ntsov.danil6556@gmail.com>
2026-06-11 15:28:09 +0500

    Add files via upload

---
+++ config.py
@@ -0,0 +1,12 @@
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+
+button1_text = "📦 Посмотреть каталог"
+button2_text = "❓ Вопросы по товару"
+button3_text = "💸 Рассчитать стоимость с допами"
+
+picture1 = "C:/Users/User/Downloads/коптильни.jpg"
